254d948f297d47f518324daf94eabb749854438e
farzana <74058963+3276farzana@users.noreply.github.com>
2024-06-15 15:53:33 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let result='47A';
+console.log(typeof result);
+let convertValueinnum= Number(result);
+console.log(typeof (convertValueinnum));
+//number
+console.log(convertValueinnum);
+//NaN= not a number;
+//47=47
+//null=0
+//47a=NaN
+
+let loged='farzana';
+let booleanloggedIn=Boolean(loged);
+console.log(booleanloggedIn);
+//1=true,0=false,
+// ""=false
+// 'farzana'=true
