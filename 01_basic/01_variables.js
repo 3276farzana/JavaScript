82c62c3f82c4109a1ea4d3d4ee45840665cebf42
farzana <74058963+3276farzana@users.noreply.github.com>
2024-06-15 11:34:47 +0000

    basic variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const account=978973;
+let email='far32@gmail.com';
+var name="farz";
+acc_city='busan';
+let accstate;
+
+// why don't we use var anymore? it's because js didn't know anything about functional and block scope before so if u worte name anywhere else it will change the name everywhere
+// with table we can wrote multiple variable together in console
+console.table([account,email,name,acc_city]);
+
