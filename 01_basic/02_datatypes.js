eda73a649a8a77a2ae35137862a24357ce88235e
farzana <74058963+3276farzana@users.noreply.github.com>
2024-06-15 15:19:14 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+'use script'; //treat all js code as newer version
+//js engine is hide in browser.
+//alert(3+3); //we are using node not browser.
+//number=>2 to power 53
+//bigint= for trading,stockmarket or big website like fb
+//symbols=for unique
+console.log(typeof 'him');
+console.log(typeof null);
+//ans: object
+console.log(typeof undefined);
+//ans: undefined
