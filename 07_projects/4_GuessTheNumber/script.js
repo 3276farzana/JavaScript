f1facf3f136e73a213b92fdfbec93fd23cc8c6c1
farzana <74058963+3276farzana@users.noreply.github.com>
2024-06-26 04:56:23 +0000

    guess the number

--- 07_projects/4_GuessTheNumber/script.js
+++ 07_projects/4_GuessTheNumber/script.js
@@ -1,3 +1,31 @@
 const randomNumber=parseInt(Math.random()*100+1);
 
-const submit=document.querySelector('.subt')
+const submit=document.querySelector('.subt');
+const userInput=document.querySelector('#guessField');
+const guessSlot=document.querySelector('.guesses');
+const remaining=document.querySelector('.lastResult');
+const lowOrHigh=document.querySelector('.lowOrHi');
+const restart=document.querySelector('.resultParas');
+const p=document.createElement('p');
+const prevGuess=[]
+const guessNum=1
+let playGame=true;
+
+function validationGuess(guess){
+
+}
+function checkGuess(guess){
+
+}
+function displayGuess(guess){
+
+}
+function displayMessage(message){
+
+}
+function endGame(){
+
+}
+function newGame(){
+    
+}
