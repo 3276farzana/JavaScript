755d6ebb4a13cd3312d17f5dde13daf67eb8f254
farzana <74058963+3276farzana@users.noreply.github.com>
2024-07-06 04:10:24 +0000

    promises

--- 09_advanced/promise.js
+++ 09_advanced/promise.js
@@ -35,22 +35,79 @@
 // })
 //  4th way
 
-const promiseFour=new Promise(function(resolve,reject){
+// const promiseFour=new Promise(function(resolve,reject){
+//     setTimeout(() => {
+//         let err=true;
+//         if (!true) {
+//             resolve({name:'farzana',age:12})
+//         }
+//         else{
+//             reject('Error: something went wrong')
+//         }
+//     }, 1000);
+// })
+// promiseFour.then((user)=>{
+//     console.log(user);
+//     return user.username
+// }).then((username)=>{
+//     console.log(username);
+// }).catch(function (err) {
+//     console.log(err);
+// })
+
+
+const promiseFive=new Promise(function(resolve,reject){
     setTimeout(() => {
         let err=true;
         if (!true) {
-            resolve({name:'farzana',age:12})
+            resolve({course:'javaScript',hours:12})
         }
         else{
-            reject('Error: something went wrong')
+            reject('Error: course faced some issue')
         }
     }, 1000);
 })
-promiseFour.then((user)=>{
-    console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch(function (err) {
-    console.log(err);
+
+async function consumePromiseFive(){
+    const response = await promiseFive
+    console.log(response);
+}
+
+// async wait directly can't handel errors
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        // const data= response.json() it won't work because it takes time to convert
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
+    }
+}
+
+getAllUsers()
+
+// then and catch away 
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
 })
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
+
+//The fetch() function will reject the promise on some errors, but not if the server responds with an error status like 404: so we also check the response status and throw if it is not OK. it won't go to resolve or reject. it's just a respose.it's a network error
