408f527f8efbc4f538c4401e2c31906a6288568e
farzana <74058963+3276farzana@users.noreply.github.com>
2024-07-06 06:48:25 +0000

    basic of oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,7 +1,45 @@
-// object literall
-const user={
-    username:'jam',
-    loginCount:8,
-    Login:true
+// // object literall
+// const user={
+//     username:'jam',
+//     loginCount:8,
+//     Login:true,
+//     getUserDetails: function () {
+//         console.log('got user data from database');
+//         console.log(`username is :${this.username}`);
+//         console.log(this);//print the whole properties
+//     }
+// }
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this); // it will show {} because it's global context, it's empty now
+
+//constructor
+
+//new is a constructor function=it helps to make new context
+//it always give new context
+
+// promise=new Promise();
+// date=new Date();
+
+function user(username,loginCount,isLoggedIn) {
+    this.gf=username
+    this.loginCount=loginCount;
+    this.isLoggedIn=isLoggedIn;
+    this.greeting=function(){
+        console.log(`welcome ${this.gf}`);
+    }
+    return this;
 }
-console.log(user.username);
+// const u1=user('gam',3,true);
+// const u2= user('ham',9,false);
+//console.log(u1);// it's value will be the value of u2,it overwrites the value of u1
+//to solve this
+const u1=new user('gam',3,true);
+const u2=new user('ham',9,false);
+console.log(u1);
+console.log(u2.constructor);//it's the reference of it's own
+
+// when you use new it will give you a empty object{}
+//2nd all you wrote will pass on this
+
+//read instance of 
