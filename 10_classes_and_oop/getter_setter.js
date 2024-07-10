939f28046cfa1d421ac14cc892bbac8fdf57f548
farzana <74058963+3276farzana@users.noreply.github.com>
2024-07-10 15:18:39 +0000

    getter and setter method

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -3,15 +3,24 @@ class user{
         this.email=email;
         this.password=password;//those are properties
     }
-    //you can add getter and setter in properties
+    //you can add getter and settrCase()er in properties
     //by default for all properties have getter and setter method
+    get email(){
+        return this._email.toLowerCase()
+        //in getter we always had to return the value
+    }
+    set email(value){
+        this._email=value
+        //in setter we don't return value
+    }
     get password(){
-        return this.password.toUpperCase()
+        return this.pass.toUpperCase() //it can't be the same name as the properties name or else it maximize the stack start race with constructor
     }
     // if you set getter then you have to add setter it's vise versa
     set password(value){
-        this.password=value
+        this.pass=value
     }
 }
-const u1=new user('u1@gmail.com',34554);
-console.log(user.password);
+const u1=new user('uASER1@gmail.com','hgfh');
+console.log(u1.password);
+console.log(u1.email);
